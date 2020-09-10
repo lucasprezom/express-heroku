577e2b12ed7abd1f2e347839cf0bec1e3c9ee9e4
lucasprezom <lucasprezom@gmail.com>
2020-09-09 21:51:01 -0300

    atualizando o index

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use(routes);
 app.use(...swaggerConfig);
 
-app.get('/', (req, res) => res.json('Health check ok'));
+app.get('/', (req, res) => res.json('Aula de Heroku funcionou TOP!'));
 
 app.listen(port, () => console.log('App running on port', port));
